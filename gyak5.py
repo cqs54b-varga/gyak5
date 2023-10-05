0184407d35cb490a47328bfc1fdad7d8fc83db35
laci_2ora <lacivarga00@gmail.com>
2023-10-05 10:19:36 +0200

    gyak5

--- gyak5.py
+++ gyak5.py
@@ -7,17 +7,18 @@ def regisztracio():
     i = 1
 while not jelszo_ellenorzes():
      print("Nem egyezik a két jelszó")
- i +=1
+     i+=1
 return sikeres
 
     def felhasznalonev():
+    felhasznalo_email = input("Kérem az email cimet")
 
     def jelszo_bekeres():
             pass
 
 def jelszo_elleorzes():
     pass
-    ok_jelszo = true
+    ok_jelszo = True
     return ok_jelszo
 
 def beleptetes():
