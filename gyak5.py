9abe594792fc876f3cbaab88c966c0d2551383c0
laci_2ora <lacivarga00@gmail.com>
2023-10-05 09:50:39 +0200

    gyak5

--- gyak5.py
+++ gyak5.py
@@ -1,1 +1,27 @@
-print('as')
+#beléptető rendszer
+
+def regisztracio():
+    sikeres = True
+    felhasznalonev()
+    jelszo_bekeres()
+    i = 1
+while not jelszo_ellenorzes():
+     print("Nem egyezik a két jelszó")
+ i +=1
+return sikeres
+
+    def felhasznalonev():
+
+    def jelszo_bekeres():
+            pass
+
+def jelszo_elleorzes():
+    pass
+    ok_jelszo = true
+    return ok_jelszo
+
+def beleptetes():
+
+
+
+
